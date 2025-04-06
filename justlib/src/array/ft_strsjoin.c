0a280b0167f8dc1fb32e58b2fdc643975b5867e0
justJuste <jules.longin1@gmail.com>
2025-04-06 12:56:00 +0200

    protect str(s)join

--- justlib/src/array/ft_strsjoin.c
+++ justlib/src/array/ft_strsjoin.c
@@ -19,20 +19,24 @@ char	**ft_strsjoin(const char **s1, const char **s2)
 	int			j;
 	char		**str;
 
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
 	str = ft_calloc(ft_strslen(s1) + ft_strslen(s2) + 1, sizeof(char *));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	if (s1)
 	{
-		str[i] = ft_strdup(s1[i]);
-		i++;
+		while (s1[i])
+		{
+			str[i] = ft_strdup(s1[i]);
+			i++;
+		}
 	}
 	j = 0;
-	while (s2[j])
-		str[i++] = ft_strdup(s2[j++]);
+	if (s2)
+		while (s2[j])
+			str[i++] = ft_strdup(s2[j++]);
 	str[i] = NULL;
 	return (str);
 }
